72a3d0f0fadce9678d14327cbd12bbe0142b6ad4
Richard Pranjatno <rpra0006@student.monash.edu>
2022-05-09 15:30:56 +1000

    Display user's current location in MapView - Added permission in Info.plist - Request permission from user - Uses San Fransisco as default location currently

--- OpenCurate/ViewController/Map/MapViewController.swift
+++ OpenCurate/ViewController/Map/MapViewController.swift
@@ -2,18 +2,53 @@
 //  MapViewController.swift
 //  OpenCurate
 //
-//  Created by Richard Pranjatno on 22/4/2022.
+//  Created by Richard Pranjatno on 9/5/2022.
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager:CLLocationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy =  kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
+        mapView.delegate = self
+        
+        if let coor = mapView.userLocation.location?.coordinate {
+            mapView.setCenter(coor, animated: true)
+        }
 
         // Do any additional setup after loading the view.
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        
+        mapView.mapType = MKMapType.standard
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: locValue, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locValue
+        annotation.title = "Current Location"
+        mapView.addAnnotation(annotation)
+        
+    }
+    
     
 
     /*
